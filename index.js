c232d57a23151f79d5c6ecf87f59e3df2dc993a8
Matthew Bartl <mattbartl@gmail.com>
2019-02-25 00:59:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var musicians = [];
-var instruments = [];
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay() {
   for (var i = 0; i < musicians.length; i++) {
