be919c4a35f19979ecbd824888ac739af72247e7
Matthew Bartl <mattbartl@gmail.com>
2019-02-25 01:13:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 function theBeatlesPlay() {
   for (var i = 0; i < musicians.length; i++) {
     return [
-      musicians[0] + " plays " + instruments[0] + ","
-      musicians[1] + " plays " + instruments[1] + ","
-      musicians[2] + " plays " + instruments[2] + ","
-      musicians[3] + " plays " + instruments[3] + ","
+      musicians[0] + " plays " + instruments[0],
+      musicians[1] + " plays " + instruments[1],
+      musicians[2] + " plays " + instruments[2],
+      musicians[3] + " plays " + instruments[3],
     ];
   }
 }
