b24752a6812bffbc2a986c2c48aff24d4af158ae
Matthew Bartl <mattbartl@gmail.com>
2019-02-25 00:54:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 var musicians = [];
 var instruments = [];
-var johnLennonFacts = [];
 
 function theBeatlesPlay() {
   for (var i = 0; i < musicians.length; i++) {
-    return musicians[i] + " plays " + instruments[i];
+    return musicians(i) + " plays " + instruments(i);
   }
 }
 
 function johnLennonFacts() {
   for (var i = 0; i < johnLennonFacts.length; i++) {
-    return johnLennonFacts[i] + "!!!";
+    return johnLennonFacts(i) + "!!!";
   }
 }
 
