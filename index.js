2e5e09087543499f11a2678b567e577cd84e4956
Matthew Bartl <mattbartl@gmail.com>
2019-02-25 01:18:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 function theBeatlesPlay() {
-var i;
-  for (i = 0; i < musicians.length; i++) {
-    return musicians[i] + " plays " + instruments[i];
-  }
+return [
+  musicians[0] + " plays " + instruments[0],
+  musicians[1] + " plays " + instruments[1],
+  musicians[2] + " plays " + instruments[2],
+  musicians[3] + " plays " + instruments[3],
+  ]
 }
 
 function johnLennonFacts() {
